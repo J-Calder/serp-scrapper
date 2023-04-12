92b8f22c11e058991962a248691ce7571b4b62a7
J-Calder <jakecalder@gmail.com>
2023-04-12 00:27:21 -0700

    Update 2

--- serp-scrapper.py
+++ serp-scrapper.py
@@ -8,12 +8,19 @@ from pymongo import MongoClient
 import pandas as pd
 import openai
 import sys
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['scraped_data']
@@ -44,127 +51,120 @@ async def analyze_text_with_chatgpt(session, text):
             if 'choices' not in response_data:
                 print(f"Unexpected API response: {response_data}")
                 return []
-            keywords = response_data["choices"][0]["text"].strip()
+            keywords = response_data['choices'][0]['text'].strip()
             return keywords.split(", ")
 
     except Exception as e:
         print(f"Error analyzing text: {e}")
         return []
 
-async def fetch(session, url, headers):
-    async with session.get(url, headers=headers, ssl=False) as response:
-        return await response.text()
+async def scrape_search_results(session, query, num_results):
+    # Set up Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-async def scrape_search_results(query, num_results):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
+    # Initialize the WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
 
     data = []
 
-    async with aiohttp.ClientSession() as session:
-        for start in range(0, num_results, 10):
-            url = f'https://www.google.com/search?q={query}&start={start}'
+    for start in range(0, num_results, 10):
+        url = f'https://www.google.com/search?q={query}&start={start}'
 
-            try:
-                response_text = await fetch(session, url, headers)
-            except Exception as e:
-                print(f"Error while making request: {e}")
-                continue
+        try:
+            driver.get(url)
 
-            soup = BeautifulSoup(response_text, 'html.parser')
-            search_results = soup.select('.tF2Cxc')
+            # Wait for the search results to load
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '.tF2Cxc'))
+            )
+
+            search_results = driver.find_elements(By.CSS_SELECTOR, '.tF2Cxc')
 
             for result in search_results:
-                title = result.select_one('.LC20lb.DKV0Md').get_text(strip=True)
+              title = result.find_element(By.CSS_SELECTOR, 'h3').text
+            try:
+                    subheader_element = result.find_element(By.CSS_SELECTOR, '.VwiC3b.yXK7lf.MUxGbd.yDYNvb.lyLwlc')
+                    subheader = subheader_element.text
+            except NoSuchElementException:
+                    subheader = None
+                    page_url = result.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+                    print(f"Scraping URL: {page_url}")
+                    # Scrape the content of the page
+                    driver.get(page_url)
 
-                subheader = result.select_one('.VwiC3b.yXK7lf.MUxGbd.yDYNvb.lyLwlc')
-                
-                if subheader:
-                    subheader = subheader.get_text(strip=True)    
-
-                page_url = result.select_one('a')['href']
-                print(f"Scraping URL: {page_url}")
-
-                try:
-                    page_response_text = await fetch(session, page_url, headers)
-                    page_soup = BeautifulSoup(page_response_text, 'html.parser')
-
-                    
-                    h1_tags = [tag.get_text(strip=True) for tag in page_soup.find_all('h1')]
-                    h2_tags = [tag.get_text(strip=True) for tag in page_soup.find_all('h2')]
-                    h3_tags = [tag.get_text(strip=True) for tag in page_soup.find_all('h3')]
+                    h1_tags = [tag.text for tag in driver.find_elements(By.TAG_NAME, 'h1')]
+                    h2_tags = [tag.text for tag in driver.find_elements(By.TAG_NAME, 'h2')]
+                    h3_tags = [tag.text for tag in driver.find_elements(By.TAG_NAME, 'h3')]
                     headings = h1_tags + h2_tags + h3_tags
 
-                    paragraphs = page_soup.find_all('p')
-
-                    # Process paragraphs
                     paragraph_keywords = []
-                    for paragraph in paragraphs[:num_paragraphs]:
-                        paragraph_text = paragraph.get_text(strip=True)
-                        if len(paragraph_text) < 50:
-                            continue
 
-                        keywords = await analyze_text_with_chatgpt(session, paragraph_text)
-                        paragraph_keywords.extend(keywords)
+                    for heading_tag in ['h1', 'h2', 'h3']:
+                       heading_elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, f"//{heading_tag}")))
 
-                except Exception as e:
-                    print(f"Error while processing result: {e}")
-                    continue
+                    for heading_element in heading_elements:
+                        if heading_element:
+                            heading_text = heading_element.text
+                            try:
+                                paragraph = heading_element.find_element(By.XPATH, './following-sibling::p')
+                            except NoSuchElementException:
+                                paragraph = None
 
-                scraped_data = {
-                    "url": page_url,
-                    "metadata": {
+                            if paragraph:
+                                paragraph_text = paragraph.text
+                                if len(paragraph_text) >= 50:
+                                    keywords = await analyze_text_with_chatgpt(session, paragraph_text)
+                                    paragraph_keywords.extend(keywords)
+
+                        scraped_data = {
+                       "url": page_url,
+                       "metadata": {
                         "title": title,
                         "subheader": subheader,
-                    },
-                    "content": {
+                       },
+                        "content": {
                         "headings": headings,
-                    },
-                }
+                       },
+                    }
 
-                chatgpt_data = {
+                    chatgpt_data = {
                     "url": page_url,
                     "analysis": {
-                        "keywords": paragraph_keywords,
+                    "keywords": paragraph_keywords,
                     },
                 }
 
-                data.append(scraped_data)
-                scraped_data_collection.insert_one(scraped_data)
-                chatgpt_data_collection.insert_one({"url": page_url, "keywords": paragraph_keywords})
+                    data.append(scraped_data)
+                    scraped_data_collection.insert_one(scraped_data)
+                    chatgpt_data_collection.insert_one({"url": page_url, "keywords": paragraph_keywords})
+
+        except Exception as e:
+            print(f"Error while processing result: {e}")
+            continue
+
+    driver.quit()  # Close the browser and quit the WebDriver
 
     return data
 
-if __name__ == "__main__":
+async def main():
     query = "cbd"
     num_results = 1
-    num_paragraphs = 3
 
-    asyncio.run(scrape_search_results(query, num_results))
+    async with aiohttp.ClientSession() as session:
+        data = await scrape_search_results(session, query, num_results)
 
-    scraped_data_cursor = scraped_data_collection.find()
-    scraped_data_df = pd.DataFrame(list(scraped_data_cursor))
-
-    print(scraped_data_df.columns)
-    # Multi-level columns for scraped_data_df
-    scraped_data_df = scraped_data_df.drop(columns=['_id'])
-    scraped_data_df.columns = pd.MultiIndex.from_tuples(
-        [("metadata", "title"), ("metadata", "subheader"), ("url", ""), ("content", "headings"), ("metadata_unused", ""), ("content_unused", "")]
-    )
-
-    chatgpt_data_cursor = chatgpt_data_collection.find()
-    chatgpt_data_df = pd.DataFrame(list(chatgpt_data_cursor))
-
-    # Adding the requested code
     scraped_data_cursor = scraped_data_collection.find({}, {"_id": 0})
     scraped_data_df = pd.DataFrame(list(scraped_data_cursor))
     scraped_data_df.to_csv("scraped_data.csv", index=False)
 
-    chatgpt_data_cursor = chatgpt_data_collection.find()
-    chatgpt_data_df = pd.DataFrame(list(chatgpt_data_cursor))
-    
-	# Adding the code to export chatgpt_data to a CSV file
     chatgpt_data_cursor = chatgpt_data_collection.find({}, {"_id": 0})
     chatgpt_data_df = pd.DataFrame(list(chatgpt_data_cursor))
     chatgpt_data_df.to_csv("chatgpt_data.csv", index=False)
+
+# Run the main function
+if __name__ == "__main__":
+    asyncio.run(main())
+
